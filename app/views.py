8ae441c62c88a8da91d30778a7faf3140eb09ce9
Matheus Manoel <matheuscmanoel@gmail.com>
2017-07-07 14:07:16 -0300

    add openid options.

--- app/views.py
+++ app/views.py
@@ -29,4 +29,7 @@ def login():
                             ))
         return redirect('/index')
 
-    return render_template('login.html', title='Sign In', form=form)
+    return render_template(
+                            'login.html', title='Sign In', form=form,
+                            providers=app.config['OPENID_PROVIDERS']
+                          )
