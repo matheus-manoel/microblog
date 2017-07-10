4386364f45aa63c6c1f89eecab2cdd113adc4b91
Matheus Manoel <matheuscmanoel@gmail.com>
2017-07-10 11:45:21 -0300

    add configuration to init.

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,19 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_openid import OpenID
+from config import basedir
+
 
 app = Flask(__name__)
 # Read and use config.py file in root folder
 app.config.from_object('config')
 # Initialize db
 db = SQLAlchemy(app)
+
+lm = LoginManager()
+lm.init_app(app)
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
 
 from app import views, models
